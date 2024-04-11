37b7856c95bf9dabc2d295b88035bce22c0014e7
juliazimmermann2023 <128699018+juliazimmermann2023@users.noreply.github.com>
2024-04-11 08:54:13 -0300

    Update conta_bancaria.py

--- conta_bancaria.py
+++ conta_bancaria.py
@@ -6,3 +6,16 @@ class ContaBancaria:
         
     def deposita (self, valor):
         self.saldo += valor
+    def exibir_detalhes(self):
+        print("Número da conta:", self.numero)
+        print("Titular:", self.titular)
+        print("Saldo:", self.saldo)
+    def sacar(self,valor):
+        if self.saldo >=valor:
+            self.saldo -=valor
+        else: 
+            print("Saldo insuficiente")
+conta_da_julia = ContaBancaria("1234-5","Júlia",100)
+conta_da_julia.deposita(500)
+conta_da_julia.sacar(200)
+conta_da_julia.exibir_detalhes()
