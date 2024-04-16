78cf2eb6b7198e5e395d4fc3b812411dec94f92e
juliazimmermann2023 <128699018+juliazimmermann2023@users.noreply.github.com>
2024-04-16 08:54:28 -0300

    Create conta_bancaria_2.py

---
+++ conta_bancaria_2.py
@@ -0,0 +1,58 @@
+#Objeto é uma unica coleção de dados(atributos) e comportamentos (metodos)
+class ContaBancaria:
+    
+    #Atributos são variaveis internas dentro do objeto
+    def __init__(self, numero, titular, saldo=0):
+        self.numero = numero
+        self.titular = titular
+        self.saldo = saldo
+
+    #Métodos são funções do obejeto que produzem algum comportamento
+    def depositar(self, valor):
+        self.saldo += valor
+
+    def exibir_detalhes(self):
+        print("Número da Conta:", self.numero)
+        print("Titular:", self.titular)
+        print("Saldo: ", round(self.saldo,2))
+
+    def sacar(self,valor):
+        if self.saldo >= valor:
+            self.saldo -= valor
+        else:
+            print("Saldo insuficiente!")
+
+def exibir_menu():
+    print("\n MENU:")
+    print("1- Exibir detalhe da conta:")
+    print("2- Realizar depósito:")
+    print("3- Realizar saque:")
+    print("0- Sair do programa:")
+
+#aqui estou criando uma instancia do objeto ContaBancaria
+#com o nome conta_do_usuario
+numero_conta = input("Digite o numero da conta:")
+titular_conta = input("Digite o titular da conta:")
+saldo_inicial = float(input("Digite o saldo inicial da conta:").replace(",","."))
+
+conta_do_usuario = ContaBancaria(numero_conta, titular_conta, saldo_inicial)
+opcao= None
+while opcao!= 0:
+    exibir_menu()
+    opcao= int(input("Digite o número da opção desejada:"))
+    if opcao ==1:
+        conta_do_usuario.exibir_detalhes()
+    elif opcao==2:
+        valor_deposito = float(input("Digite o valor a ser depositado:").replace(",","."))
+        conta_do_usuario.depositar(valor_deposito)
+    elif opcao==3:
+        valor_saque = float(input("Digite o valor a ser sacado:").replace(",","."))
+        conta_do_usuario.sacar(valor_saque)
+#Usando os metodos do objeto ContaBancaria
+
+
+
+
+
+conta_do_usuario.exibir_detalhes()
+
